982a388fe61b53e6c02040e721c3dfb6a257d810
Manmeet Kaur <manmeet.kaur@quincus.com>
2026-05-06 09:53:30 +0530

    Update Json file path

--- src/test/java/com/utility/JsonUtility.java
+++ src/test/java/com/utility/JsonUtility.java
@@ -13,7 +13,7 @@ public class JsonUtility {
 	public static Environment readJson(Env env)
 	{
 		Gson gson=new Gson();
-		File jsonFile=new File(System.getProperty("user.dir")+"\\config\\config.json");
+		File jsonFile=new File(System.getProperty("user.dir")+File.separator+"config"+File.separator+"config.json");
 		FileReader fileReader=null;
 		try {
 			fileReader = new FileReader(jsonFile);
